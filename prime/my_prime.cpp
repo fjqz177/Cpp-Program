021bbe5cd156fd6844be11c0a4c9468db01be99b
fjqz177 <fjqz177@139.com>
2023-02-05 20:32:15 +0800

    对代码进行格式化操作

--- prime/my_prime.cpp
+++ prime/my_prime.cpp
@@ -1,15 +1,13 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 #include <windows.h>
 using namespace std;
 
-bool lbbl(int n, vector<int> pl)//判断是否为质数的函数
+bool lbbl(int n, vector<int> pl) // 判断是否为质数的函数
 {
     int i = 1;
-    while ((pl[i - 1]) * (pl[i - 1]) <= n)
-    {
-        if (n % (pl[i - 1]) == 0)
-        {
+    while ((pl[i - 1]) * (pl[i - 1]) <= n) {
+        if (n % (pl[i - 1]) == 0) {
             return false;
         }
         i++;
@@ -17,32 +15,29 @@ bool lbbl(int n, vector<int> pl)//判断是否为质数的函数
     return true;
 }
 
-int main()
-{
+int main() {
     int n = 3;
     int geshu;
-    cin>>geshu;
-    int geshu2 = int(geshu/2);
+    cin >> geshu;
+    int geshu2 = int(geshu / 2);
     vector<int> pl = {2};
-    //计时模块
-    LARGE_INTEGER t1,t2,tc;
+    // 计时模块
+    LARGE_INTEGER t1, t2, tc;
     QueryPerformanceFrequency(&tc);
     QueryPerformanceCounter(&t1);
-    for(int z=0; z<geshu2-1; z++)
-    {
-        if (lbbl(n, pl))
-        {
+    for (int z = 0; z < geshu2 - 1; z++) {
+        if (lbbl(n, pl)) {
             pl.push_back(n);
-            //cout << n <<endl;
+            // cout << n <<endl;
         }
         n += 2;
     }
-    //计时模块
+    // 计时模块
     QueryPerformanceCounter(&t2);
-    double time=(double)(t2.QuadPart-t1.QuadPart)/(double)tc.QuadPart;
-    
-    cout << "time = " << time << " s" << endl;  //输出时间（单位：ｓ）
-    cout << "前" << geshu << "内共有 " << pl.size() << "个质数" <<endl;  //输出共有的质数数量
-    system("pause");//防止闪退
+    double time = (double)(t2.QuadPart - t1.QuadPart) / (double)tc.QuadPart;
+
+    cout << "time = " << time << " s" << endl;                           // 输出时间（单位：ｓ）
+    cout << "前" << geshu << "内共有 " << pl.size() << "个质数" << endl; // 输出共有的质数数量
+    system("pause");                                                     // 防止闪退
     return 0;
 }
