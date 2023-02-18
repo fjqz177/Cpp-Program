02ed57f25a53c710afcaf761c68dfeee562b36fa
fjqz177 <fjqz177@139.com>
2023-02-19 00:16:04 +0800

    更改vector<int>为vector<bool>  计算完后自动释放内存

--- prime/aishi_prime.cpp
+++ prime/aishi_prime.cpp
@@ -2,30 +2,31 @@
 #include <vector>
 #include <windows.h>
 using namespace std;
-
+// 编译指令 g++ -O3 aishi_prime.cpp -o aishi_prime.exe
 int main() {
     int ans = 0; // 用来计数
     int nei;     // 多少以内
     cout << "请问你要计算多少以内的质数个数：";
     cin >> nei;
-    vector<int> sushu(nei + 1, 1); // 自然数从0开始，所以多记一个0，要+1
+    vector<bool> sushu(nei + 1, true); // 自然数从0开始，所以多记一个0，要+1
 
     // 计时模块
     LARGE_INTEGER t1, t2, tc;
     QueryPerformanceFrequency(&tc);
     QueryPerformanceCounter(&t1);
 
-    sushu[0] = sushu[1] = 0;             // 1 0 不是素数
+    sushu[0] = sushu[1] = false;             // 1 0 不是素数
     for (int i = 3; i <= nei; i = i + 2) // 从2开始往后筛
     {
         if (sushu[i]) {
             ans++; // 如果是素数 就计数
             for (int j = 2 * i; j <= nei; j += i) {
-                sushu[j] = 0;
+                sushu[j] = false;
             }
         }
     }
-
+    vector<bool>().swap(sushu); //释放内存
+    
     // 计时模块
     QueryPerformanceCounter(&t2);
     double time = (double)(t2.QuadPart - t1.QuadPart) / (double)tc.QuadPart;
