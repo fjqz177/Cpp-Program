bb48f21ae1f5c7e8417e953b786ef7ee652e5479
fjqz177 <fjqz177@139.com>
2023-02-19 11:54:54 +0800

    更新编译指令

--- smallpt/smallpt/smallpt.cpp
+++ smallpt/smallpt/smallpt.cpp
@@ -1,5 +1,5 @@
 #include <math.h>   // smallpt, a Path Tracer by Kevin Beason, 2008
-#include <stdlib.h> // Make : g++ -O3 -fopenmp smallpt.cpp -o smallpt
+#include <stdlib.h> // Make : g++ -O3 -fopenmp -W smallpt.cpp -o smallpt 
 #include <stdio.h>  //        Remove "-fopenmp" for g++ version < 4.2
 #include <random>
 #include <iostream>
